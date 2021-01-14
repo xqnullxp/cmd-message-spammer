f2663d9b7f5c25d23a1f42ea08d943a9d343f779
null <75458417+xqnullxp@users.noreply.github.com>
2021-01-13 19:42:41 -0600

    Add files via upload

---
+++ cmd message spammer.py
@@ -0,0 +1,21 @@
+#wrote by null
+#kys
+#imma big booter haxxer man
+
+import time
+
+msg = input("Enter the message: ")
+n = input("How many times ?: ")
+
+print("running in 5 seconds")
+
+count = 5
+while(count != 0):
+	print(count)
+	time.sleep(1)
+	count -= 1
+
+print("goodnight folks that's all")
+
+for i in range(0,int(n)):
+	print(msg + 'n') 
